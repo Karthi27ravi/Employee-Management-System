bcee030ac57e1d0da9ae36367d04a838db4cfb32
karthikeyan <karthikeyan2721999@gmail.com>
2026-02-28 17:38:09 +0530

    phase-4

--- src/main/java/org/example/ems_new/Exception/GlobalExceptionHandler.java
+++ src/main/java/org/example/ems_new/Exception/GlobalExceptionHandler.java
@@ -3,6 +3,7 @@ package org.example.ems_new.Exception;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -53,9 +54,27 @@ public class GlobalExceptionHandler {
         return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
     }
 
+    @ExceptionHandler(BadCredentialsException.class)
+    public ResponseEntity<ErrorCode> handleBadCredentials(
+            BadCredentialsException ex,
+            HttpServletRequest request) {
+
+        ErrorCode error = new ErrorCode(
+                LocalDateTime.now(),
+                HttpStatus.UNAUTHORIZED.getReasonPhrase(),
+                "Invalid username or password",
+                HttpStatus.UNAUTHORIZED.value(),
+                request.getRequestURI()
+        );
+
+        return new ResponseEntity<>(error, HttpStatus.UNAUTHORIZED);
+    }
+
     @ExceptionHandler(Exception.class)
     public ResponseEntity<ErrorCode> handleGenricError(Exception exception, HttpServletRequest request){
         return new ResponseEntity<ErrorCode>(new ErrorCode(LocalDateTime.now(), HttpStatus.INTERNAL_SERVER_ERROR.getReasonPhrase(), exception.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR.value(), request.getRequestURI() ), HttpStatus.INTERNAL_SERVER_ERROR);
     }
+
+
 
 }
