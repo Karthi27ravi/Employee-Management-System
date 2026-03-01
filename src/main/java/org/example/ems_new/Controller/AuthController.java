1584f28ca09bc61ef59e3ee57211800ccf533857
karthi27ravi <karthikeyan2721999@gmail.com>
2026-03-01 20:29:59 +0530

    phase 4

---
+++ src/main/java/org/example/ems_new/Controller/AuthController.java
@@ -0,0 +1,50 @@
+package org.example.ems_new.Controller;
+
+import lombok.RequiredArgsConstructor;
+import org.example.ems_new.Entity.AuthRequest;
+import org.example.ems_new.Entity.AuthResponse;
+import org.example.ems_new.Entity.User;
+import org.example.ems_new.Repository.UserRepository;
+import org.example.ems_new.Security.JwtUtil;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/auth")
+@RequiredArgsConstructor
+public class AuthController {
+
+    private final AuthenticationManager authenticationManager;
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final JwtUtil jwtUtil;
+
+    @PostMapping("/register")
+    public String register(@RequestBody User user) {
+
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        userRepository.save(user);
+
+        return "User Registered Successfully";
+    }
+
+    @PostMapping("/login")
+    public AuthResponse login(@RequestBody AuthRequest request) {
+
+        authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        request.getUsername(),
+                        request.getPassword()
+                )
+        );
+
+        String token = jwtUtil.generateToken(request.getUsername());
+
+        return new AuthResponse(token);
+    }
+}
---
+++ src/main/java/org/example/ems_new/Entity/AuthRequest.java
@@ -0,0 +1,11 @@
+package org.example.ems_new.Entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class AuthRequest {
+    private String username;
+    private String password;
+}
